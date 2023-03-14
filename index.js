6577ad37e7b94c17162fc2956c5c89770fa6b70d
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-14 07:45:41 -0300

    [COMMENTED] commented code logic

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 function App(){
-
+    {/*Two state variables are declared. The first one, to hold the input value. The second one, to hold the answer value*/}
     const [expression, setExpression] = React.useState('');
     const [answer, setAnswer] = React.useState(0)
 
-    const display = (symbol) => {
-        setExpression((prev) => prev + symbol);
+    {/*The display function takes in the value of the clicked button to add it to the expression value through the setExpression function.
+    It is ckecked if it's last character is an equal sign. If this is true, it is tested using a regex expression whether the penultimate
+    character of the new expression value is either a number or a '.' (to display it on screen without the answer value ), or  an operation
+    symbol (to concatenate it with the answer)*/}
+    const display = (char) => {
+        setExpression((prev) => prev + char);
         if(expression[expression.length - 1] == '='){
-            if(/[0-9.]/.test(symbol)){
-                setExpression(symbol);
+            if(/[0-9.]/.test(char)){
+                setExpression(char);
             }else{
-                setExpression(answer + symbol);
+                setExpression(answer + char);
             }
         }
     };
-
+    {/*This function takes in the expression input, applies the eval JS function on it (which makes the calculations) and returns a value. Then, concatenation is used to update setExpression*/}
     const calculate = () => {
         setAnswer(eval(expression));
         setExpression(prev => prev + '=');
 
     };
-
+    {/*Reset state function for the clear button*/}
     const clear = () =>{
         setExpression('');
         setAnswer(0);
@@ -29,9 +33,11 @@ function App(){
     <div className='container'>
         <div className='grid'>
             <div id='display'>
+                {/*The input has to be disabled in order to simulate a calculator*/}
                 <input type='text' value={expression} placeholder='0' disabled />
                 <div className='total'>{answer}</div>
             </div>
+            {/*Each button is attached to an event listener onClick to trigger it's function*/}
             <div onClick={clear} className='button' id='clear'>AC</div>
             <div onClick={() => display('/')} className='button' id='divide'>/</div>
             <div onClick={() => display('*')} className='button' id='multiply'>x</div>
@@ -54,4 +60,7 @@ function App(){
     )
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App/>
+);
